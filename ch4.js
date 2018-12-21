edb820c437b4f14ada99e0e22fb619313fa12841
David Zulaica <zulaica@gmail.com>
2018-12-20 21:51:03 -0800

    Ch 4, `nth()`

--- ch4.js
+++ ch4.js
@@ -70,9 +70,19 @@ const listToArray = list => {
 
 const prepend = (value, list) => ({ value, rest: list });
 
+const nth = (list, index) => {
+  if (index === 0) {
+    return !list ? undefined : list.value;
+  } else {
+    return nth(list.rest, index - 1);
+  }
+};
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
