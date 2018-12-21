fd640ffd41538a5bf90a7b5d1c6e24e8be3066bf
David Zulaica <zulaica@gmail.com>
2018-12-21 00:46:29 -0800

    Ch 5, `loop()`

--- ch5.js
+++ ch5.js
@@ -5,3 +5,18 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 
 console.log(arrays.reduce((array, value) => array.concat(value)));
 // → [1, 2, 3, 4, 5, 6]
+
+/*
+ * Your Own Loop
+ */
+const loop = (value, test, update, body) => {
+  if (test(value)) {
+    body(value);
+    return loop(update(value), test, update, body);
+  }
+};
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
